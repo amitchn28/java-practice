3129ae8f106f2df5bf0426b50264d0e6db787f16
Amit Chauhan <amitchn28@gmail.com>
2024-11-01 13:52:32 +0530

    added message

--- pattern/Pattern1.java
+++ pattern/Pattern1.java
@@ -5,13 +5,18 @@ class Pattern1{
         
         Scanner sc = new Scanner(System.in);
         
+        System.out.println("Enter Number");
         int n =sc.nextInt();
+
+
 
         if(n%2==0){
             System.out.println("Even");
         }else{
             System.out.println("Odd");
         }
+
+        
 
 
         sc.close();
